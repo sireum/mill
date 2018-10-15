70a81535a5e62335fae604f8d8df492cc88f999b
Robby <robby@k-state.edu>
2018-10-15 01:48:38 -0500

    Made sh compatible.

--- abspath.sh
+++ abspath.sh
@@ -1,15 +1,11 @@
 abspath() {
     if [ -d "$1" ]; then
-        # dir
         (cd "$1"; pwd)
     elif [ -f "$1" ]; then
-        # file
-        if [[ $1 = /* ]]; then
-            echo "$1"
-        elif [[ $1 == */* ]]; then
-            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
-        else
-            echo "$(pwd)/$1"
-        fi
+        case "$1" in
+          /*)  echo "$1";;
+          */*) echo "$(cd "${1%/*}"; pwd)/${1##*/}";;
+          *)   echo "$(pwd)/$1"
+        esac
     fi
 }
