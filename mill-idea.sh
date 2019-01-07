8d01accb2440db360d528c9318fb2156a46fcdd5
Robby <robby@k-state.edu>
2019-01-07 12:50:48 -0600

    More Windows fixes.

--- mill-idea.sh
+++ mill-idea.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+export SCRIPT_DIR=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
 export SIREUM_SOURCE_BUILD=true
-mill $* mill.scalalib.GenIdea/idea
+if [ -f ${SCRIPT_DIR}/mill.bat ]; then
+  MILL=mill.bat
+else
+  MILL=mill
+fi
+${MILL} $* mill.scalalib.GenIdea/idea
--- mill-up.sh
+++ mill-up.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-mill mill.scalalib.Dependency/updates
+export SCRIPT_DIR=$( cd "$( dirname "$0" )" &> /dev/null && pwd )
+if [ -f ${SCRIPT_DIR}/mill.bat ]; then
+  MILL=mill.bat
+else
+  MILL=mill
+fi
+${mill} mill.scalalib.Dependency/updates
